acbef9aa13e71e82a616b5868432e04b3695d8c2
willydekeyser <ma074172@skynet.be>
2023-11-14 09:08:42 +0100

    Setup

---
+++ SpringAuthorizationServerExtraLogin/src/main/java/willydekeyser/SpringAuthorizationServerExtraLoginApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationServerExtraLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationServerExtraLoginApplication.class, args);
+	}
+
+}
---
+++ SpringAuthorizationServerExtraLogin/src/main/java/willydekeyser/config/MyUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package willydekeyser.config;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	public static final String SPRING_SECURITY_FORM_TEST_KEY = "test";
+	
+	public MyUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+	
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) 
+        throws AuthenticationException {
+
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+
+        UsernamePasswordAuthenticationToken authRequest = getAuthRequest(request);
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthRequest(HttpServletRequest request) {
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+        String test = obtainTest(request);
+
+        if (username == null) {
+            username = "";
+        }
+        if (password == null) {
+            password = "";
+        }
+        if (test == null) {
+            test = "";
+        }
+        if (!test.equals("TEST")) {
+        	throw new BadCredentialsException("Test not found!");
+		}
+        return new UsernamePasswordAuthenticationToken(username, password);     
+        
+    }
+
+    private String obtainTest(HttpServletRequest request) {
+        return request.getParameter(SPRING_SECURITY_FORM_TEST_KEY);
+    }
+}
